5a7ecef1ebda0e7dd605189c270e75a9d3b09779
Fredrik Ahlström <fredrikahlstrom75@gmail.com>
2023-04-28 18:10:32 +0200

    Fixed bios defaults.

--- source/Cart.s
+++ source/Cart.s
@@ -112,12 +112,12 @@ ROM_Space:
 //	.incbin "wsroms/XI Little (Japan).wsc"
 ROM_SpaceEnd:
 WS_BIOS_INTERNAL:
-	.incbin "wsroms/boot.rom"
-//	.incbin "wsroms/ws_irom.bin"
+//	.incbin "wsroms/boot.rom"
+	.incbin "wsroms/ws_irom.bin"
 WSC_BIOS_INTERNAL:
 SC_BIOS_INTERNAL:
-	.incbin "wsroms/boot1.rom"
-//	.incbin "wsroms/wc_irom.bin"
+//	.incbin "wsroms/boot1.rom"
+	.incbin "wsroms/wc_irom.bin"
 //	.incbin "wsroms/wsc_irom.bin"
 
 	.section .ewram,"ax"
